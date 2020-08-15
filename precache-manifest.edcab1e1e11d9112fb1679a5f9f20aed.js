cd516c4a81601bcfa9dd6a059e2917d586fc555d
nikcich <64556428+nikcich@users.noreply.github.com>
2020-08-15 12:41:10 -0500

    Updates

---
+++ precache-manifest.edcab1e1e11d9112fb1679a5f9f20aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376e1145dbfb9de9d6ed5549dceedc24",
+    "url": "/YammieMeetupV2/index.html"
+  },
+  {
+    "revision": "e33eeef1bdad6ef79689",
+    "url": "/YammieMeetupV2/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "e18c72c3905e94f9d4cd",
+    "url": "/YammieMeetupV2/static/css/main.2c5754a8.chunk.css"
+  },
+  {
+    "revision": "e33eeef1bdad6ef79689",
+    "url": "/YammieMeetupV2/static/js/2.8744f39a.chunk.js"
+  },
+  {
+    "revision": "a5adfca3f4c7256e8d7d29e5afd08a9e",
+    "url": "/YammieMeetupV2/static/js/2.8744f39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18c72c3905e94f9d4cd",
+    "url": "/YammieMeetupV2/static/js/main.689fc355.chunk.js"
+  },
+  {
+    "revision": "49653c8b8795b2fe9b2b",
+    "url": "/YammieMeetupV2/static/js/runtime-main.ddd4992a.js"
+  }
+]);
